9aaed28d14d5faeb9961e1c7fb82c95d6922850d
Kozim <kozim_7484443@mail.ru>
2024-05-20 16:59:52 +0500

    task 1 finished

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -4,9 +4,12 @@ import api from "../api";
 const Users = () => {
   const [users, setUsers] = useState(api.users.fetchAll());
 
-  return (
-    <>
-      <h1 className="badge bg-primary">12 человек тусанет с тобой сегодня</h1>
+  const handleDelete = (userId) => {
+    setUsers((users) => users.filter((user) => user._id !== userId));
+  };
+
+  const UsersTable = () => {
+    return (
       <table className="table text-center">
         <thead>
           <tr>
@@ -22,17 +25,39 @@ const Users = () => {
           {users.map((user) => (
             <tr key={user._id}>
               <th scope="row">{user.name}</th>
-              <td>{user.qualities.map((qualities, id) => {
-                return <span key={id} className={`badge bg-${qualities.color} m-1`}>{qualities.name}</span>
-              })}</td>
+              <td>
+                {user.qualities.map((qualities, id) => {
+                  return (
+                    <span
+                      key={id}
+                      className={`badge bg-${qualities.color} m-1`}
+                    >
+                      {qualities.name}
+                    </span>
+                  );
+                })}
+              </td>
               <td>{user.profession.name}</td>
               <td>{user.completedMeetings}</td>
               <td>{user.rate} / 5</td>
-              <td><span className="btn btn-danger">Удалить</span></td>
+              <td onClick={() => handleDelete(user._id)}>
+                <span className="btn btn-danger">Удалить</span>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
+    );
+  };
+
+  return (
+    <>
+      <h1 className={`btn btn-${users.length ? "primary" : "danger"}`}>
+        {users.length
+          ? `${users.length} человек тусанет с тобой сегодня`
+          : "Никто с тобой не тусанёт"}
+      </h1>
+      {users.length ? <UsersTable /> : null}
     </>
   );
 };
