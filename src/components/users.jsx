800d325d33321c546005cae8a0f240e75f5a181c
Kozim <kozim_7484443@mail.ru>
2024-05-20 16:36:44 +0500

    task 1 finished

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -7,7 +7,7 @@ const Users = () => {
   return (
     <>
       <h1 className="badge bg-primary">12 человек тусанет с тобой сегодня</h1>
-      <table className="table">
+      <table className="table text-center">
         <thead>
           <tr>
             <th scope="col">Имя</th>
@@ -15,16 +15,20 @@ const Users = () => {
             <th scope="col">Профессия</th>
             <th scope="col">Встретился, раз</th>
             <th scope="col">Оценка</th>
+            <th scope="col"></th>
           </tr>
         </thead>
         <tbody>
           {users.map((user) => (
             <tr key={user._id}>
               <th scope="row">{user.name}</th>
-              <td>{user.qualities.map(qualities => qualities.name)}</td>
+              <td>{user.qualities.map((qualities, id) => {
+                return <span key={id} className={`badge bg-${qualities.color} m-1`}>{qualities.name}</span>
+              })}</td>
               <td>{user.profession.name}</td>
               <td>{user.completedMeetings}</td>
-              <td>{user.rate}</td>
+              <td>{user.rate} / 5</td>
+              <td><span className="btn btn-danger">Удалить</span></td>
             </tr>
           ))}
         </tbody>
